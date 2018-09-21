076daad98c0e9a7baedca16aa21cf1bf8915dac2
Mark Wissink <mwissink.b@gmail.com>
2018-09-21 16:16:13 -0400

    Working on homework1

---
+++ homework01/app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const = [
+];
+app.get('/', (req, res) => res.sendStatus(404));
+
+iapp.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
