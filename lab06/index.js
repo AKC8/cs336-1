2afb706ec5571124b7ea9d35c7b4dc6e7f767833
Mark Wissink <mwissink.b@gmail.com>
2018-10-10 17:19:17 -0400

    Finished lab06

--- lab06/index.js
+++ lab06/index.js
@@ -6,6 +6,8 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use('/', express.static(__dirname + '/public'));
+
 const writeResponseWithBody = (req, res, method) => {
   res.status(200);
   res.send(`Hello ${method} - ${JSON.stringify(req.body)}`);
@@ -21,6 +23,8 @@ app.get('/request', (req, res) => writeResponse(req, res, 'GET'))
 .put('/request', (req, res) => writeResponseWithBody(req, res, 'PUT'))
 .delete('/request', (req, res) => writeResponse(req, res, 'DELETE'))
 .head('/request', (req, res) => writeResponse(req, res, 'HEAD'))
-.all('/request', (req, res) => res.sendStatus(501))
+.all('/request', (req, res) => res.sendStatus(501));
+
+app.post('/forms', (req, res) => writeResponseWithBody(req, res, 'POST'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
